f4d82580240a7748b489a5c7c6c6eda5b9953f26
Max Linke <maxlinke88@gmail.com>
2025-07-20 17:41:05 +0200

    eval 0.1.14
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/eval.rb
+++ Formula/eval.rb
@@ -1,8 +1,8 @@
 class Eval < Formula
   desc "A CLI tool for employee evaluations with automatic evidence collection"
   homepage "https://github.com/kain88-de/eval"
-  url "https://github.com/kain88-de/eval/releases/download/v0.1.12/eval-x86_64-unknown-linux-gnu.tar.gz"
-  sha256 "b7b55e65c78ff5f8b96424238fb0b8c0057798572da1ea2d4446f28e1b49775e"
+  url "https://github.com/kain88-de/reviewr/releases/download/v0.1.14/reviewr-x86_64-unknown-linux-gnu.tar.gz"
+  sha256 "2dfc26fc7f2e1f0f504be157e1dcdef3fe8cad121baa72ac99319ad94660c257"
   license "MIT"
 
   def install
