00cdf79098d13dec7c7c7287502ef29e2e9c6269
Max Linke <maxlinke88@gmail.com>
2025-07-22 10:16:37 +0200

    reviewr 1.0.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/reviewr.rb
+++ Formula/reviewr.rb
@@ -1,9 +1,9 @@
 class Reviewr < Formula
   desc "CLI tool for employee reviews with notes and evidence tracking"
   homepage "https://github.com/kain88-de/reviewr"
-  url "https://github.com/kain88-de/reviewr/releases/download/v1.0.3/reviewr-x86_64-unknown-linux-gnu.tar.gz"
-  sha256 "69bcb08d9a40322db512344cf80f929e3cb7676e42ebb3d8f586dbd95f060815"
-  version "1.0.3"
+  url "https://github.com/kain88-de/reviewr/releases/download/v1.0.4/reviewr-x86_64-unknown-linux-gnu.tar.gz"
+  sha256 "9646d470f48102bc8b094610f654b57a2b25ff3baf8affe15768c1e493942946"
+  version "1.0.4"
 
   def install
     bin.install "reviewr"
